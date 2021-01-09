cc0902fd5ce90366215d0b62af0a7191389cfb64
Raphael Augusto Pereira de Assis <rafadastan02@live.com>
2021-01-09 12:13:05 -0300

    terminando a 1 parte de frontend

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -9,7 +9,7 @@ function Footer(){
         <footer className="main-footer">
             App desenvolvido para aplicativo delivery. Projeto em andamento
             <div className="footer-icons">
-                <a href="http://youtube.com.br" target="_new"> <YoutubeIcon /></a>
+                <a href="http://youtube.com.br" target="_new"><YoutubeIcon /></a>
                 <a href="http://linkedin.com.br" target="_new"><LinkedinIcon /></a>
                 <a href="http://instagram.com.br" target="_new"><InstagramIcon /></a>
             </div>
