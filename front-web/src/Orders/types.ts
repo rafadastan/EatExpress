e249d94058c9d9e9a71a6ecf165a7a1789d39a8e
Raphael Augusto Pereira de Assis <rafadastan02@live.com>
2021-01-09 20:18:59 -0300

    finish frontend

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -7,8 +7,16 @@ export type Product = {
 }
 
 
-export type OrderLocationdata = {
+export type OrderLocationData = {
     latitude: number;
     longitude: number;
     address: string;
 }
+
+type ProductId = {
+    id: number;
+}
+
+export type OrderPayload = {
+    products: ProductId[];
+} & OrderLocationData;
